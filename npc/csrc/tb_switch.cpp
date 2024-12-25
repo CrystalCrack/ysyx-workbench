750a398faf192964d8484b0c77e93777855f02b7
tracer-ysyx <tracer@ysyx.org>
2024-12-25 21:07:44 +0800

    > sim RTL ysyx_12345678 颜晨曦 Linux CPC 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:07:44 up  3:34,  3 users,  load average: 0.05, 0.39, 0.46

--- npc/csrc/tb_switch.cpp
+++ npc/csrc/tb_switch.cpp
@@ -10,7 +10,7 @@
 int sim_time = 0;
 int main(int argc,char **argv){
 	Vswitch *dut = new Vswitch;
-
+	Verilated::traceEverOn(true)
 	VerilatedContext *contextp = new VerilatedContext;
 	contextp->commandArgs(argc, argv);
 	VerilatedVcdC *tfp = new VerilatedVcdC;//init vcd file pointer
@@ -29,7 +29,7 @@ int main(int argc,char **argv){
 
 		tfp->dump(sim_time);
 		contextp->timeInc(1);
-		
+
 		sim_time++;
 	}
 	delete dut;

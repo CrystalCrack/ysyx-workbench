f9c539d0a54d831a19818887b2267a32ff7b2abf
tracer-ysyx <tracer@ysyx.org>
2024-12-25 22:14:33 +0800

    > sim RTL ysyx_12345678 颜晨曦 Linux CPC 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:14:33 up  4:41,  3 users,  load average: 0.12, 0.32, 0.40

--- npc/sim/obj_dir/Vlight.mk
+++ npc/sim/obj_dir/Vlight.mk
@@ -2,9 +2,9 @@
 # DESCRIPTION: Verilator output: Makefile for building Verilated archive or executable
 #
 # Execute this makefile from the object directory:
-#    make -f Vswitch.mk
+#    make -f Vlight.mk
 
-default: Vswitch
+default: Vlight
 
 ### Constants...
 # Perl executable (from $PERL)
@@ -30,9 +30,9 @@ VM_SC_TARGET_ARCH = linux
 
 ### Vars...
 # Design prefix (from --prefix)
-VM_PREFIX = Vswitch
+VM_PREFIX = Vlight
 # Module prefix (from --prefix)
-VM_MODPREFIX = Vswitch
+VM_MODPREFIX = Vlight
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
 
@@ -53,7 +53,7 @@ VM_USER_DIR = \
 
 ### Default rules...
 # Include list of all generated classes
-include Vswitch_classes.mk
+include Vlight_classes.mk
 # Include global rules
 include $(VERILATOR_ROOT)/include/verilated.mk
 
@@ -68,7 +68,7 @@ tb_switch.o: /home/crystc/ysyx-workbench/npc/csrc/tb_switch.cpp
 	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
 
 ### Link rules... (from --exe)
-Vswitch: $(VK_USER_OBJS) $(VK_GLOBAL_OBJS) $(VM_PREFIX)__ALL.a $(VM_HIER_LIBS)
+Vlight: $(VK_USER_OBJS) $(VK_GLOBAL_OBJS) $(VM_PREFIX)__ALL.a $(VM_HIER_LIBS)
 	$(LINK) $(LDFLAGS) $^ $(LOADLIBES) $(LDLIBS) $(LIBS) $(SC_LIBS) -o $@
 
 

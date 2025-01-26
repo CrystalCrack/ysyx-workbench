ccecf3c9e60e5e5e14e48e8e34ce742a8310ddb6
YCX <CrystalCracks@outlook.com>
2025-01-26 23:30:30 +0800

    expand the expression analysis and implement the watchpoint function.

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -30,5 +30,15 @@ void isa_reg_display() {
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
+  int i;
+  for(i=0;i<ARRLEN(regs);i++){
+    if(strcmp(s,regs[i])==0){
+      *success = true;
+      return cpu.gpr[i];
+    }
+  }
+  if(i==ARRLEN(regs)){
+    *success = false;
+  }
   return 0;
 }

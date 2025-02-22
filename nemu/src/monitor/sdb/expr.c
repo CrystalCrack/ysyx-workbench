acc90753907d99150a59743d4a8a276227287167
tracer-ysyx <tracer@ysyx.org>
2025-02-22 12:23:20 +0800

    > compile NEMU ysyx_12345678 颜晨曦 Linux CPC 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:23:20 up 14 min,  1 user,  load average: 1.06, 0.53, 0.36

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -410,10 +410,16 @@ word_t expr(char *e, bool *success) {
 
   /* Check the token '*' and '-' */
   for(int i=0;i<nr_token;i++){
-    if(tokens[i].type=='*' && (i==0 || !((tokens+i-1)->type == TK_DEC || (tokens+i-1)->type == TK_HEX || (tokens+i-1)->type == ')'))){
+    if(tokens[i].type=='*' && (i==0 || !((tokens+i-1)->type == TK_DEC || 
+                                         (tokens+i-1)->type == TK_HEX || 
+                                         (tokens+i-1)->type == TK_REG ||
+                                         (tokens+i-1)->type == ')'))){
       tokens[i].type = TK_DEREF;
     }
-    if(tokens[i].type=='-' && (i==0 || !((tokens+i-1)->type == TK_DEC || (tokens+i-1)->type == TK_HEX || (tokens+i-1)->type == ')'))){
+    if(tokens[i].type=='-' && (i==0 || !((tokens+i-1)->type == TK_DEC ||
+                                         (tokens+i-1)->type == TK_HEX ||
+                                         (tokens+i-1)->type == TK_REG ||
+                                         (tokens+i-1)->type == ')'))){
       tokens[i].type = TK_NEG;
     }
   }

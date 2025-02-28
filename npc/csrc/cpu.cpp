e42959ee136a7c3fb33e8bc8e298aca6c6be4e6a
tracer-ysyx <tracer@ysyx.org>
2025-03-01 02:23:39 +0800

    > sim RTL ysyx_25020057 颜晨曦 Linux CPC 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:23:39 up 37 min,  4 users,  load average: 0.21, 0.47, 0.48

--- npc/csrc/cpu.cpp
+++ npc/csrc/cpu.cpp
@@ -6,3 +6,43 @@
 #include "Vnpc___024root.h"
 #include "Vnpc__Dpi.h"
 
+int halt = 0;
+Vnpc *top;
+
+void ebreak(){
+  halt = 1;
+}
+
+
+int sim_time = 0;
+VerilatedVcdC *m_trace = new VerilatedVcdC;
+
+void single_cycle(Vnpc *top) {
+  top->clk = 1; top->eval();m_trace->dump(sim_time);sim_time++;
+  top->clk = 0; top->eval();m_trace->dump(sim_time);sim_time++;
+}
+
+void reset(Vnpc *top, int n) {
+  top->rst = 1;
+  while (n > 0) {
+    single_cycle(top);
+    n--;
+  }
+  top->rst = 0;
+}
+
+void init_cpu(){
+    Vnpc *dut = new Vnpc;
+    Verilated::traceEverOn(true);
+    dut->trace(m_trace, 10);
+    m_trace->open("npc.vcd");
+    reset(dut,10);
+    while(!halt) {
+        dut->inst = pmem_read(dut->pc);
+        single_cycle(dut);
+    }
+    std::cout << "simulation ended" << std::endl;
+    m_trace->close();
+    delete m_trace;
+    delete dut;
+}

95b5be500830a4f90d43ac328b4f364f0a2dfd86
tracer-ysyx <tracer@ysyx.org>
2025-03-01 13:45:57 +0800

    > sim RTL ysyx_25020057 颜晨曦 Linux CPC 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:45:57 up  4:56,  4 users,  load average: 0.70, 0.53, 0.54

--- npc/vsrc/IDU.v
+++ npc/vsrc/IDU.v
@@ -36,9 +36,9 @@ module IDU(
     assign immS = {{20{inst[31]}}, inst[31:25], inst[11:7]};
     assign immB = {{20{inst[31]}}, inst[7], inst[30:25], inst[11:8], 1'b0};
     assign immJ = {{12{inst[31]}}, inst[19:12], inst[20], inst[30:21], 1'b0};
-    assign rs1 = inst_is_ebreak ? 5'd0 : inst[19:15];
+    assign rs1 = stop_sim ? 5'd0 : inst[19:15];
     assign rs2 = inst[24:20];
-    assign rd = inst_is_ebreak ? 10 : inst[11:7];
+    assign rd = stop_sim ? 10 : inst[11:7];
     assign funct3 = inst[14:12];
     assign funct7 = inst[31:25];
     assign opcode = inst[6:0];

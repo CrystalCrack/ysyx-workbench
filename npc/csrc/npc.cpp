f79c0493d5e10ba039518bec50c0bd057e3e81d3
tracer-ysyx <tracer@ysyx.org>
2025-03-01 10:42:42 +0800

    > sim RTL ysyx_25020057 颜晨曦 Linux CPC 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:42:42 up  1:52,  4 users,  load average: 0.69, 0.60, 0.53

--- npc/csrc/npc.cpp
+++ npc/csrc/npc.cpp
@@ -4,23 +4,23 @@
 
 uint32_t pmem_read(uint32_t addr);
 
-// Cpu cpu("npc.vcd");
-Vnpc *dut = new Vnpc;
-VerilatedVcdC *m_trace = new VerilatedVcdC;
+Cpu cpu("npc.vcd");
+// Vnpc *dut = new Vnpc;
+// VerilatedVcdC *m_trace = new VerilatedVcdC;
 
 void ebreak(){
   // cpu.stop();
 }
 
 int main(int argc, char **argv) {
-  Verilated::traceEverOn(true);
-  dut->trace(m_trace, 10);
-  m_trace->open("npc.vcd");
-  // std::cout<<"NPC simulation starts"<<std::endl;
-  // while(cpu.state() == RUN && cpu.sim_time < MAX_SIM_TIME) {  
-  //   cpu.dut->inst = pmem_read(cpu.dut->pc);
-  //   cpu.single_cycle();
-  // }
-  // std::cout << "simulation ended" << std::endl;
+  // Verilated::traceEverOn(true);
+  // dut->trace(m_trace, 10);
+  // m_trace->open("npc.vcd");
+  std::cout<<"NPC simulation starts"<<std::endl;
+  while(cpu.state() == RUN && cpu.sim_time < MAX_SIM_TIME) {  
+    cpu.dut->inst = pmem_read(cpu.dut->pc);
+    cpu.single_cycle();
+  }
+  std::cout << "simulation ended" << std::endl;
   return 0;
 }

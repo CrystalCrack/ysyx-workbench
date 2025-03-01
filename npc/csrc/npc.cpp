95d5ca77b8bc3dff588287a4bc3c6679b16565b4
tracer-ysyx <tracer@ysyx.org>
2025-03-01 12:24:10 +0800

    > sim RTL ysyx_25020057 颜晨曦 Linux CPC 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:24:10 up  3:34,  4 users,  load average: 0.95, 0.96, 0.70

--- npc/csrc/npc.cpp
+++ npc/csrc/npc.cpp
@@ -33,7 +33,7 @@ void parse_args(int argc, char **argv) {
 
 int main(int argc, char **argv) {
 
-  extern void get_reg(int *reg_data, int addr);
+  extern void get_reg(int addr, int* reg_data);
   svSetScope(svGetScopeFromName("TOP.RegisterFile"));
 
   cpu_init("npc.vcd");

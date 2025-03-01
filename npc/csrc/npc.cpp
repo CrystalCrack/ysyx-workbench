141bcfefa9a5721c4139b41987a866307a1e0b98
tracer-ysyx <tracer@ysyx.org>
2025-03-01 13:40:33 +0800

    > sim RTL ysyx_25020057 颜晨曦 Linux CPC 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:40:33 up  4:50,  4 users,  load average: 0.59, 0.54, 0.57

--- npc/csrc/npc.cpp
+++ npc/csrc/npc.cpp
@@ -23,7 +23,7 @@ void parse_args(int argc, char **argv) {
     {NULL, 0, NULL, 0}
   };
   int o;
-  while((o = getopt_long(argc, argv, "-i", table, NULL)) != -1) {
+  while((o = getopt_long(argc, argv, "-i:", table, NULL)) != -1) {
     switch(o) {
       case 'i':
         std::cout << "image file: " << optarg << std::endl;

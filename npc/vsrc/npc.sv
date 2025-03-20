cfa7292c87a0df30f1012498c663373f830acaf9
tracer-ysyx <tracer@ysyx.org>
2025-03-20 22:47:37 +0800

    > sim RTL ysyx_25020057 颜晨曦 Linux CPC 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:47:37 up  9:25,  3 users,  load average: 1.08, 0.75, 0.62

--- npc/vsrc/npc.sv
+++ npc/vsrc/npc.sv
@@ -39,7 +39,7 @@ module npc(
         .rdata(instF),
         .rresp(),
         .rvalid(validF),
-        .rready(ifetch_en)
+        .rready(readyD)
     );
 
     /* -------------------------------------------------------------------- */

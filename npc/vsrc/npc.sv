b58044b80f353eda5a66094070cee3251e4925e8
tracer-ysyx <tracer@ysyx.org>
2025-03-20 22:41:57 +0800

    > sim RTL ysyx_25020057 颜晨曦 Linux CPC 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:41:57 up  9:19,  3 users,  load average: 0.87, 0.70, 0.55

--- npc/vsrc/npc.sv
+++ npc/vsrc/npc.sv
@@ -505,7 +505,7 @@ module npc(
     export "DPI-C" function is_inst_done;
     function void is_inst_done();
         output int done;
-        done = {31'b0, validW};
+        done = {31'b0, validW & readyF};
     endfunction
 
 endmodule
